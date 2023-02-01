275319ad85a0d24dcb6b84471a979c63bd47a7b1
NGN-Limjihoon <lonelynight1026@gmail.com>
2023-02-01 18:03:29 +0900

    관심사의 분리: 생성자 주입

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    /* MemberService가 직접 어떤 구현체를 설정 X 
+    *   => AppConfig가 그 역할 할당을 해주자
+    * */
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+    }
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,19 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    MemberServiceImpl가 어떤 구현체를 사용할지 직접 결정
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    외부에서 어떤 구현체를 사용할지 결정해줌. MemberServiceImpl는 다형성에 대한 Act만 충실히 해주면 됨!
+    => SRP!!
+    */
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemoryMemberRepository memoryMemberRepository) {
+        this.memberRepository = memoryMemberRepository;
+    }
+
     /*인터페이스 변수 => 구현체 객체
     * :: 인터페이스와 구현체 모두 사용
     * => 인터페이스와 구현체 둘 다 의존!!!
