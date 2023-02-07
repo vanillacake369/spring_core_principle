24b50ba02d386371eeff445b943f5be35a703d4a
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-02-07 21:07:04 +0900

    스프링 빈 컨테이너,빈 등록

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,21 +1,40 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     /* MemberService가 직접 어떤 구현체를 설정 X 
-    *   => AppConfig가 그 역할 할당을 해주자
+    *   :: AppConfig가 그 역할 할당을 해주자
+    *   ==> "생성자 주입"
     * */
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+    // 생성자 주입에 관련된 메서드는 각 행동이 뚜렷히 보이게 리팩토링하도록 한다.
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+    // 할인정책에 대해 바꾸고 싶다면 아래 메서드 한 줄만 바꿔주면 된다.
+    @Bean
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
+
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        /*MemberService 인터페이스를 구현한 memberService*/
+//        1.
 //        MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService();
+//        2.
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         /* member생성하고 join*/
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
