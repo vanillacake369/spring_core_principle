8d4c5c0963c5bac03d8bb5fd05db9def975b2e93
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-05-01 21:24:03 +0900

    빈 조회 관련 메서드 배우기

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -27,6 +27,7 @@ public class ApplicationContextBasicFindTest {
     @Test
     @DisplayName("구체 타입으로 조회")
     void findBeanByName2(){
+        // 유연성이 떨어짐. (상속관계에서 타입으로 조회하는 것이 지양해야함)
         MemberService memberService = ac.getBean( "memberService",MemberServiceImpl.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
@@ -36,6 +37,7 @@ public class ApplicationContextBasicFindTest {
     void findBeanByNameX(){
         // ac.getBean("xxxx", MemberService.class)
 
+        // xxxx로 빈 조회할 때 NoSuchBeanDefinitionException 예외를 던지면 테스트 통과   
         org.junit.jupiter.api.Assertions.assertThrows(
                 NoSuchBeanDefinitionException.class,
                 () -> ac.getBean("xxxx", MemberService.class)
